a72f8655c754e2293f9be8b0cd294e31ab4ef190
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-28 16:44:27 -0600

    includes Layout and adds next/head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import Head from 'next/head';
 import PropTypes from 'prop-types';
+// components
+import Layout from '@/components/Layout';
 // styles
 import '../styles/globals.css';
 
@@ -8,7 +11,19 @@ MyApp.propTypes = {
 };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Head>
+        <title>Bert&#39;s Painting &#38; Decorating</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta
+          name='description'
+          content='Winnipeg&#39;s top painters and painting services. Inquire about our interior painting, exterior painting and decorating expertise.'
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
 export default MyApp;
