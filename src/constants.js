6dfcd5a9706d950de8dba48cd6bd79637d1b9274
Colin Richardson <colin.richardson.dev@gmail.com>
2021-12-29 16:20:31 -0600

    adds company info constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  COMPANY_NAME: "Bert's Painting & Decorating",
+  COMPANY_PHONE_MAIN: '(204) 233-5201',
+  COMPANY_PHONE_CELL: '(204) 782-4314',
+  COMPANY_ADDRESS_STREET: '135 Birchdale Ave.',
+  COMPANY_ADDRESS_CITY: 'Winnipeg',
+  COMPANY_ADDRESS_PROVINCE: 'Manitoba',
+  COMPANY_ADDRESS_POSTAL: 'R2H 1S3',
+};
