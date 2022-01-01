d386dce1121d4adb5c2d78934486842a1db92f03
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-01 16:46:23 -0600

    refactor to remove consuming of other components for styling

--- components/Logo.js
+++ components/Logo.js
@@ -1,36 +1,24 @@
 import Image from 'next/image';
 import styled from 'styled-components';
-// components for styling
-import { LogoWrapper as NavLogoWrapper } from '@/components/layout/Nav';
 // constants
-import { COMPANY_NAME, mediaQueries } from '@/src/constants';
+import { COMPANY_NAME } from '@/src/constants';
 // assets
 import logoImage from '@/public/images/logo.webp';
 
 export default function Logo() {
   return (
-    <ImageWrapper mediaQueries={mediaQueries}>
+    <div>
       <StyledImage
         src={logoImage}
         alt={`${COMPANY_NAME} logo`}
         quality={100}
         priority
       />
-    </ImageWrapper>
+    </div>
   );
 }
 
 // styles
 const StyledImage = styled(Image)`
   width: 100%;
-`;
-const ImageWrapper = styled.div`
-  ${NavLogoWrapper} & {
-    @media (min-width: ${({ mediaQueries }) => mediaQueries.mobileS}) {
-      width: 150px;
-    }
-    @media (min-width: ${({ mediaQueries }) => mediaQueries.tablet}) {
-      width: 200px;
-    }
-  }
 `;
