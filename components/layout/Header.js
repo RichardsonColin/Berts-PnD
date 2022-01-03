c5148488e4f94a9890114d764f337bf7313c8b6c
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-03 13:24:19 -0600

    refactors Header to exclude BorderSpacer component

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,60 +1,91 @@
-import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-// hooks
-import useWindowDimensions from '@/hooks/useWindowDimensions';
+// components
+import Logo from '@/components/Logo';
+import BorderSpacer from '@/components/ui/BorderSpacer';
 // constants
 import { mediaQueries } from '@/src/constants';
 
 Header.propTypes = {
   children: PropTypes.node.isRequired,
-  spacer: PropTypes.number,
 };
 
 export default function Header(props) {
-  const { width } = useWindowDimensions();
-  const defaultSpacerHeight = 3;
-  const [spacerHeight, setSpacerHeight] = useState(defaultSpacerHeight);
-
-  useEffect(() => {
-    // resize the header's bottom spacer for smaller viewports
-    const breakPointWidth = Number(mediaQueries.laptop.replace('px', ''));
-    const newSpacerHeight =
-      width > breakPointWidth ? defaultSpacerHeight : defaultSpacerHeight - 1;
-    return () => {
-      setSpacerHeight(newSpacerHeight);
-    };
-  });
-
   return (
-    <StyledHeader spacerHeight={spacerHeight} mediaQueries={mediaQueries}>
-      {props.children}
+    <StyledHeader id='header' mediaQueries={mediaQueries}>
+      <BorderSpacer size={3} position='bottom' />
+      <ContentWrapper>
+        <LogoWrapper mediaQueries={mediaQueries}>
+          <Link href='/' passHref>
+            <LogoLink>
+              <Logo />
+            </LogoLink>
+          </Link>
+        </LogoWrapper>
+        {props.children}
+      </ContentWrapper>
     </StyledHeader>
   );
 }
 
 // styles
-const StyledHeader = styled.header`
+const ContentWrapper = styled.div``;
+export const StyledHeader = styled.header`
   position: fixed;
   width: 100%;
-  height: ${({ spacerHeight }) => `${spacerHeight + 50}px`};
-  min-height: ${({ spacerHeight }) => `${spacerHeight + 50}px`};
+  height: 42px;
+  min-height: 42px;
   background-color: #fff;
   box-shadow: 0px 1px 2px 1px var(--color-grey-100);
+  z-index: 1;
 
-  &:before {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    height: ${({ spacerHeight }) => `${spacerHeight}px`};
-    background: var(--primary);
-    content: '';
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.tablet}) {
+    height: 50px;
+    min-height: 50px;
+  }
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.laptop}) {
+    height: 90px;
+    min-height: 90px;
   }
 
+  & ${ContentWrapper} {
+    position: relative;
+    height: inherit;
+    margin: 0 auto;
+    @media (min-width: ${({ mediaQueries }) => mediaQueries.laptop}) {
+      max-width: 1350px;
+    }
+    @media (max-width: 1350px) {
+      padding-right: 1rem;
+    }
+  }
+`;
+const LogoLink = styled.a``;
+const LogoWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 130px;
+  height: 100%;
+
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.tablet}) {
+    width: 150px;
+  }
   @media (min-width: ${({ mediaQueries }) => mediaQueries.laptop}) {
-    height: ${({ spacerHeight }) => `${spacerHeight + 90}px`};
-    min-height: ${({ spacerHeight }) => `${spacerHeight + 90}px`};
+    width: 200px;
+    justify-content: flex-start;
+  }
+  @media (max-width: 1350px) {
+    padding-left: 1rem;
+  }
+
+  & ${LogoLink} {
+    line-height: 0;
   }
 `;
