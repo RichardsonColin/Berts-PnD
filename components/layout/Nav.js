c180c978c109e20a05c9a0cbb8e4464b6df71c12
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-03 13:25:44 -0600

    refactors to exclude content that belongs in Header

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,23 +1,19 @@
 import { useState, useRef } from 'react';
-import { useRouter } from 'next/router';
 import FocusLock from 'react-focus-lock';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 // hooks
 import useMediaQuery from '@/hooks/useMediaQuery';
 import useClickOutside from '@/hooks/useClickOutside';
 // components
-import Logo from '@/components/Logo';
 import NavList from './NavList';
-import Burger from '@/components/ui/Burger';
-import BurgerMenu from '@/components/ui/BurgerMenu';
+import Burger from '@/components/layout/Burger';
+import BurgerMenu from '@/components/layout/BurgerMenu';
 // constants
-import { siteRoutes, mediaQueries } from '@/src/constants';
+import { mediaQueries } from '@/src/constants';
 
 export default function Nav() {
   const [open, setOpen] = useState(false);
   const node = useRef();
-  const router = useRouter();
-  const currentRoute = router.route;
   const breakPointWidth = Number(mediaQueries.laptop.replace('px', ''));
   const isBreakPoint = useMediaQuery(breakPointWidth);
 
@@ -25,24 +21,16 @@ export default function Nav() {
   useClickOutside(node, () => setOpen(false));
   return (
     <>
-      <LogoWrapper mediaQueries={mediaQueries}>
-        <Logo />
-      </LogoWrapper>
       {isBreakPoint ? (
-        <div ref={node}>
+        <MobileNavWrapper ref={node}>
           <FocusLock disabled={!open}>
             <Burger open={open} setOpen={setOpen} />
-            <BurgerMenu
-              routes={siteRoutes}
-              currentRoute={currentRoute}
-              open={open}
-              setOpen={setOpen}
-            />
+            <BurgerMenu open={open} setOpen={setOpen} />
           </FocusLock>
-        </div>
+        </MobileNavWrapper>
       ) : (
-        <StyledNav>
-          <NavList routes={siteRoutes} currentRoute={currentRoute} />
+        <StyledNav aria-label='Main'>
+          <NavList />
         </StyledNav>
       )}
     </>
@@ -50,55 +38,7 @@ export default function Nav() {
 }
 
 // styles
-// TODO: modify image to have border
-const StyledBorder = css`
-  padding: 8px 8px 5px 7px;
-  background: linear-gradient(to right, var(--primary) 0.2em, transparent 0.2em)
-      0 100%,
-    linear-gradient(to left, var(--primary) 0.2em, transparent 0.2em) 100% 0,
-    100%,
-    linear-gradient(to bottom, var(--primary) 0.2em, transparent 0.2em) 100% 0,
-    linear-gradient(to top, var(--primary) 0.2em, transparent 0.2em) 0 100%,
-    100%;
-  background-repeat: no-repeat;
-  background-size: 50% 50%;
-  overflow: hidden;
-
-  @media (min-width: 1024px) {
-    background: linear-gradient(
-          to right,
-          var(--primary) 0.15em,
-          transparent 0.15em
-        )
-        0 100%,
-      linear-gradient(to left, var(--primary) 0.2em, transparent 0.2em) 100% 0,
-      100%,
-      linear-gradient(to bottom, var(--primary) 0.15em, transparent 0.15em) 100%
-        0,
-      linear-gradient(to top, var(--primary) 0.15em, transparent 0.15em) 0 100%,
-      100%;
-    background-repeat: no-repeat;
-    background-size: 50% 50%;
-    overflow: hidden;
-  }
-`;
-const LogoWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-
-  /* ${StyledBorder} */
-
-  @media (min-width: ${({ mediaQueries }) => mediaQueries.laptop}) {
-    justify-content: flex-start;
-  }
-`;
+const MobileNavWrapper = styled.div``;
 const StyledNav = styled.nav`
   display: flex;
   justify-content: flex-end;
