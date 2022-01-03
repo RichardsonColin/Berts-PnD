4141ffb2b4bcbe75efbfd991190325675efc0c29
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-03 13:22:37 -0600

    adds mobile hamburger menu

---
+++ components/layout/Burger.js
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+Burger.propTypes = {
+  open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
+};
+
+export default function Burger({ open, setOpen }) {
+  const isExpanded = open ? true : false;
+  return (
+    <StyledBurger
+      aria-label='Toggle mobile navigation'
+      aria-expanded={isExpanded}
+      open={open}
+      onClick={() => setOpen(!open)}
+    >
+      <span />
+      <span />
+      <span />
+    </StyledBurger>
+  );
+}
+
+// styles
+const StyledBurger = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  span {
+    position: relative;
+    width: 2rem;
+    height: 0.25rem;
+    background-color: ${({ open }) =>
+      open ? css`var(--secondary)` : css`var(--primary)`};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? '0' : '1')};
+      transform: ${({ open }) => (open ? 'translateX(20px)' : 'translateX(0)')};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+    }
+  }
+`;
---
+++ components/layout/BurgerMenu.js
@@ -0,0 +1,130 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+// hooks
+import useCurrentRoute from '@/hooks/useCurrentRoute';
+// components
+import QuoteLinkButton from '@/components/QuoteLinkButton';
+// constants
+import { siteRoutes, mediaQueries } from '@/src/constants';
+
+BurgerMenu.propTypes = {
+  open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
+};
+
+export default function BurgerMenu({ open, setOpen }) {
+  const currentRoute = useCurrentRoute();
+  const isHidden = open ? true : false;
+  const tabIndex = isHidden ? 0 : -1;
+  return (
+    <StyledBurgerMenu
+      aria-label='Main'
+      aria-hidden={!isHidden}
+      open={open}
+      mediaQueries={mediaQueries}
+    >
+      <MenuList>
+        {siteRoutes.map((route) => (
+          <MenuItem key={route} mediaQueries={mediaQueries}>
+            <Link href={route} passHref>
+              <StyledLink
+                isCurrentRoute={route === currentRoute}
+                tabIndex={tabIndex}
+                onClick={() => setOpen(!open)}
+              >
+                {route === '/' ? 'home' : route.replace('/', '')}
+              </StyledLink>
+            </Link>
+          </MenuItem>
+        ))}
+        <MenuItem mediaQueries={mediaQueries}>
+          <QuoteLinkButton />
+        </MenuItem>
+      </MenuList>
+    </StyledBurgerMenu>
+  );
+}
+
+// styles
+const MenuItem = styled.li``;
+const MenuList = styled.ul``;
+const StyledBurgerMenu = styled.nav`
+  position: absolute;
+  top: 42px;
+  right: -1px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  min-width: 50%;
+  min-height: 100vh;
+  padding: 1.5rem 1rem;
+  background-color: var(--primary);
+  text-align: right;
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
+
+  & ${MenuList} {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  & ${MenuList} ${MenuItem} {
+    margin: 1rem 0;
+
+    /* Landscape on devices up to laptops */
+    @media (max-width: ${({ mediaQueries }) =>
+        mediaQueries.laptop}) and (max-height: 500px) {
+      margin-bottom: 0.55rem 0;
+    }
+  }
+
+  /* min-widths */
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.mobileS}) {
+    font-size: 1em;
+  }
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.mobileL}) {
+    font-size: 1.25em;
+  }
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.tablet}) {
+    top: 50px;
+    min-width: 33%;
+    font-size: 1.25em;
+  }
+  @media (min-width: ${({ mediaQueries }) => mediaQueries.laptop}) {
+    font-size: 2em;
+  }
+  /* max-widths */
+  /* Landscape on smaller devices up to tablets */
+  @media (max-width: ${({ mediaQueries }) =>
+      mediaQueries.tablet}) and (max-height: 400px) {
+    font-size: 1em;
+  }
+`;
+const StyledLink = styled.a`
+  text-transform: capitalize;
+  font-weight: bold;
+  letter-spacing: 0.2rem;
+  color: var(--secondary);
+  text-decoration: none;
+  transition: color 0.3s linear;
+
+  ${({ isCurrentRoute }) =>
+    isCurrentRoute
+      ? css`
+          color: var(--color-grey-50);
+          padding-left: 10px;
+          border-left: 4px solid var(--primary-light);
+          border-bottom: 2px solid var(--primary-light);
+        `
+      : ''}
+
+  &:hover {
+    color: var(--color-grey-50);
+  }
+`;
