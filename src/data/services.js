67500f8d83d283f50392ed9036807b904ced3389
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-10 17:42:02 -0600

    adds mock data for services

---
+++ src/data/services.js
@@ -0,0 +1,34 @@
+// assets
+import interiorImage from '@/public/images/hero-primary.webp';
+import interiorIcon from '@/public/images/paint-roller.svg';
+
+export const servicesData = [
+  {
+    heading: 'Interior Painting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    image: interiorImage,
+    icon: interiorIcon,
+  },
+  {
+    heading: 'Decorating',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    image: interiorImage,
+    icon: interiorIcon,
+  },
+  {
+    heading: 'Exterior Painting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    image: interiorImage,
+    icon: interiorIcon,
+  },
+  {
+    heading: 'Accents',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    image: interiorImage,
+    icon: interiorIcon,
+  },
+];
