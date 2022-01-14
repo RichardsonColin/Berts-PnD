3751bc555d1f5b032ad8e00894d30addf5f27044
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-14 13:40:40 -0600

    adds data for reuseable component use

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+// assets
+import productIconOne from '@/public/images/paint-roller.svg';
+import productIconTwo from '@/public/images/paint-roller.svg';
+import productIconThree from '@/public/images/paint-roller.svg';
+
+export const productsData = [
+  {
+    heading: 'Paint',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    icon: productIconOne,
+  },
+  {
+    heading: 'Wall Paper',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    icon: productIconTwo,
+  },
+  {
+    heading: 'Primer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    icon: productIconThree,
+  },
+];
---
+++ src/data/renovations.js
@@ -0,0 +1,25 @@
+// assets
+import renovationIconOne from '@/public/images/paint-roller.svg';
+import renovationIconTwo from '@/public/images/paint-roller.svg';
+import renovationIconThree from '@/public/images/paint-roller.svg';
+
+export const renovationsData = [
+  {
+    heading: 'Interior Painting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    icon: renovationIconOne,
+  },
+  {
+    heading: 'Decorating',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    icon: renovationIconTwo,
+  },
+  {
+    heading: 'Commercial',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+    icon: renovationIconThree,
+  },
+];
