a13755c5ce55d23a43104d1b07b2676a27c9f5a3
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-14 13:34:21 -0600

    modifies to pass children to LinkButton

--- components/HeroPrimary.js
+++ components/HeroPrimary.js
@@ -20,7 +20,7 @@ export default function HeroPrimary() {
           <HeroHeading>
             Professional Painters For Your Home or Business
           </HeroHeading>
-          <QuoteLinkButton text='Schedule Now' />
+          <QuoteLinkButton>Schedule Now</QuoteLinkButton>
         </HeroContent>
       </HeroContentWrapper>
       <HeroImageWrapper>
