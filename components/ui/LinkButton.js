09c5faea8bd7ce25e43dfb1e30e2753d8ec17ab4
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-14 13:39:47 -0600

    removes unecessary wrapper elm

--- components/ui/LinkButton.js
+++ components/ui/LinkButton.js
@@ -16,53 +16,48 @@ export default function LinkButton({
   className = '',
 }) {
   return (
-    <LinkButtonWrapper>
-      <Link href={href} passHref>
-        <StyledLinkButton className={className} isPrimary={isPrimary}>
-          {children}
-        </StyledLinkButton>
-      </Link>
-    </LinkButtonWrapper>
+    <Link href={href} passHref>
+      <StyledLinkButton className={className} isPrimary={isPrimary}>
+        {children}
+      </StyledLinkButton>
+    </Link>
   );
 }
 
 // styles
-const LinkButtonWrapper = styled.div`
-  display: inline-block;
-`;
 const StyledLinkButton = styled.a`
-  ${LinkButtonWrapper} & {
-    display: inline-block;
-    padding: 1rem 2rem;
-    border-radius: 3px;
-    font-size: 1em;
-    font-weight: 500;
-    box-shadow: 1px 1px 2px 0px var(--color-grey-800);
-    background-color: var(--secondary);
-    transition: 0.2s ease;
-    text-shadow: 1px 1px var(--color-grey-990);
+  display: inline-block;
+  width: 175px;
+  padding: 1rem 0;
+  border-radius: 2px;
+  font-size: 1em;
+  font-weight: 500;
+  text-align: center;
+  box-shadow: 1px 1px 2px 0px var(--color-grey-800);
+  background-color: var(--secondary);
+  transition: 0.2s ease;
+  text-shadow: 1px 1px var(--color-grey-990);
 
-    ${({ isPrimary }) => {
-      if (isPrimary) {
-        return css`
-          background-color: var(--primary);
-          color: var(--color-grey-900);
+  ${({ isPrimary }) => {
+    if (isPrimary) {
+      return css`
+        background-color: var(--primary);
+        color: var(--color-grey-900);
 
-          &:hover {
-            background-color: var(--primary-dark);
-            color: var(--color-grey-50);
-          }
-        `;
-      } else {
-        return css`
-          background-color: var(--secondary);
-          color: var(--color-grey-10);
+        &:hover {
+          background-color: var(--primary-dark);
+          color: var(--color-grey-50);
+        }
+      `;
+    } else {
+      return css`
+        background-color: var(--secondary);
+        color: var(--color-grey-10);
 
-          &:hover {
-            background-color: var(--secondary-accent);
-          }
-        `;
-      }
-    }}
-  }
+        &:hover {
+          background-color: var(--secondary-accent);
+        }
+      `;
+    }
+  }}
 `;
