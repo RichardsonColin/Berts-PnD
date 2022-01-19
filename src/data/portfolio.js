5e9579331d13dd4651344750ad7fa59738014f80
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-19 11:19:37 -0600

    adds initial data

---
+++ src/data/portfolio.js
@@ -0,0 +1,17 @@
+// assets
+import portfolioImage from '@/public/images/hero-primary.webp';
+
+export const portfolioData = [
+  {
+    image: portfolioImage,
+  },
+  {
+    image: portfolioImage,
+  },
+  {
+    image: portfolioImage,
+  },
+  {
+    image: portfolioImage,
+  },
+];
---
+++ src/data/process.js
@@ -0,0 +1,22 @@
+export const processData = [
+  {
+    heading: 'Schedule An Estimate',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+  },
+  {
+    heading: 'Customize Your Project',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+  },
+  {
+    heading: 'Pick Your New Colours',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+  },
+  {
+    heading: "Enjoy Your Home's New Look!",
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+  },
+];
