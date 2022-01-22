04811452dcc4a1ad2188cd862c11297ac912ca47
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-22 14:53:47 -0600

    adds mock reviews data

---
+++ src/data/reviews.js
@@ -0,0 +1,12 @@
+export const reviewsData = [
+  {
+    author: 'Thomas Turnberry',
+    review:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+  },
+  {
+    author: 'Julia Gulia',
+    review:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis neque vitae congue rhoncus. Suspendisse feugiat quam tellus.',
+  },
+];
