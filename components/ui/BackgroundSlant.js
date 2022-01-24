a6241af11f17306db026db37824e17ee230c6aaf
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-24 13:25:06 -0600

    adds prop to adjust for a full height and a slightly reduced height

--- components/ui/BackgroundSlant.js
+++ components/ui/BackgroundSlant.js
@@ -6,12 +6,22 @@ import { mediaQueries } from '@/src/constants';
 BackgroundSlant.propTypes = {
   slant: PropTypes.string,
   degree: PropTypes.number,
+  isStandardHeight: PropTypes.bool,
 };
 
 // add position: relative and z-index : 1 on container where this is used.
-export default function BackgroundSlant({ slant = 'left', degree = 20 }) {
+export default function BackgroundSlant({
+  slant = 'left',
+  degree = 20,
+  isStandardHeight = true,
+}) {
+  const heightMultiple = isStandardHeight ? 1 : 0.8;
   return (
-    <StyledBackgroundSlant slant={slant} degree={`${parseInt(degree)}deg`} />
+    <StyledBackgroundSlant
+      slant={slant}
+      degree={`${parseInt(degree)}deg`}
+      heightMultiple={heightMultiple}
+    />
   );
 }
 
@@ -31,14 +41,14 @@ const StyledBackgroundSlant = styled.div`
     left: 50%;
     display: block;
     width: 1000px;
-    height: 240px;
+    height: 350px;
     background-color: var(--secondary-light);
     opacity: 1;
     z-index: -1;
     transform: ${({ slant, degree }) => {
       let rotate = slant === 'left' ? `-${degree}` : degree;
       return css`
-        translate(-50%, -65%) rotate(${rotate})
+        translate(-50%, -55%) rotate(${rotate})
       `;
     }};
     transform-origin: center;
@@ -47,17 +57,20 @@ const StyledBackgroundSlant = styled.div`
     /* custom breakpoint */
     @media (min-width: 600px) {
       width: 3000px;
-      height: 418px;
+      height: ${({ heightMultiple }) =>
+        `${parseInt(Math.ceil(heightMultiple * 418))}px`};
     }
     @media (min-width: ${mediaQueries.tablet}) {
-      height: 520px;
+      height: ${({ heightMultiple }) =>
+        `${parseInt(Math.ceil(heightMultiple * 520))}px`};
     }
     @media (min-width: ${mediaQueries.laptop}) {
-      height: 785px;
+      height: ${({ heightMultiple }) =>
+        `${parseInt(Math.ceil(heightMultiple * 768))}px`};
       transform: ${({ slant, degree }) => {
         let rotate = slant === 'left' ? `-${degree}` : degree;
         return css`
-          translate(-50%, -55%) rotate(${rotate});
+          translate(-50%, -48%) rotate(${rotate});
         `;
       }};
     }
