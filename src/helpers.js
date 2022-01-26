93f0fd71bb48beff3ca5dded1de17ef05f2e4f55
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-25 19:28:40 -0600

    adds helper for parsing path for file name

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,15 @@
-export function searchString(string, pattern) {
+export function searchString(string, regExpPattern) {
   try {
-    const regex = new RegExp(pattern);
-    const result = regex.exec(string);
-    return result !== null ? result[0] : result;
+    if (typeof string === 'string' && string.length) {
+      const regex = new RegExp(regExpPattern);
+      const result = regex.exec(string);
+      // first index of a RegExp.exec return value is the matched expression
+      return result !== null ? result[0] : result;
+    }
+    // default value for unmatched RegExp.exec expression
+    return null;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return null;
   }
 }
