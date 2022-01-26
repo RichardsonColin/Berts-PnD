bf715bb0ec89b34e85ae1e58520759dced2c7661
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-25 18:21:36 -0600

    adds Experience section data source

---
+++ src/data/experience.js
@@ -0,0 +1,25 @@
+// assets
+import ratingIcon from '@/public/images/rating.svg';
+
+export const experienceData = [
+  {
+    value: '5/5',
+    text: 'Rating',
+    icon: ratingIcon,
+  },
+  {
+    value: '100%',
+    text: 'Satisfaction',
+    icon: ratingIcon,
+  },
+  {
+    value: "1000's",
+    text: 'Jobs Completed',
+    icon: ratingIcon,
+  },
+  {
+    value: '2',
+    text: 'Craftsmen',
+    icon: ratingIcon,
+  },
+];
