7c204cf8b439f95ea5d18589b23a57a2a2eb4f67
Colin Richardson <colin.richardson.dev@gmail.com>
2022-01-25 19:28:07 -0600

    adds site wide constants

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,27 @@
-module.exports = {
-  COMPANY_NAME: "Bert's Painting & Decorating",
-  COMPANY_PHONE_MAIN: '(204) 233-5201',
-  COMPANY_PHONE_CELL: '(204) 782-4314',
-  COMPANY_ADDRESS_STREET: '135 Birchdale Ave.',
-  COMPANY_ADDRESS_CITY: 'Winnipeg',
-  COMPANY_ADDRESS_PROVINCE: 'Manitoba',
-  COMPANY_ADDRESS_POSTAL: 'R2H 1S3',
-  COMPANY_DESCRIPTION:
-    "Bert's Painting & Decorating is a father and son company providing service in English and French to many satisfied customers for over 35 years. We are Red Seal Qualified Journeyman Painters providing interior and exterior work in residential and small commercial buildings.",
-  sitePages: ['home', 'about', 'services', 'testimonials', 'contact'],
-  mediaQueries: {
-    mobileS: '320px',
-    mobileM: '375px',
-    mobileL: '425px',
-    tablet: '768px',
-    laptop: '1024px',
-    laptopL: '1440px',
-    desktop: '2560px',
-  },
+export const COMPANY_NAME = "Bert's Painting & Decorating";
+export const COMPANY_OWNER = 'Bert Laroche';
+export const COMPANY_EMAIL = 'bert@bertspainting.ca';
+export const COMPANY_PHONE_MAIN = '204-233-5201';
+export const COMPANY_PHONE_CELL = '204-782-4314';
+export const COMPANY_ADDRESS_STREET = '135 Birchdale Ave.';
+export const COMPANY_ADDRESS_CITY = 'Winnipeg';
+export const COMPANY_ADDRESS_PROVINCE = 'Manitoba';
+export const COMPANY_ADDRESS_POSTAL = 'R2H 1S3';
+export const COMPANY_DESCRIPTION =
+  "Bert's Painting & Decorating is a father and son company providing service in English and French to many satisfied customers for over 35 years. We are Red Seal Qualified Journeyman Painters providing interior and exterior work in residential and small commercial buildings.";
+export const siteRoutes = [
+  '/',
+  '/about',
+  '/services',
+  '/testimonials',
+  '/contact',
+];
+export const mediaQueries = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
 };
