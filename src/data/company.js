dbf24028a64074f4cfcecb06f56460edc4993c83
Colin Richardson <colin.richardson.dev@gmail.com>
2022-02-15 18:42:18 -0600

    adds company data source

---
+++ src/data/company.js
@@ -0,0 +1,26 @@
+// contants
+import {
+  COMPANY_NAME,
+  COMPANY_OWNER,
+  COMPANY_EMAIL,
+  COMPANY_PHONE_MAIN,
+  COMPANY_PHONE_CELL,
+  COMPANY_ADDRESS_STREET,
+  COMPANY_ADDRESS_CITY,
+  COMPANY_ADDRESS_PROVINCE,
+  COMPANY_ADDRESS_POSTAL,
+  COMPANY_DESCRIPTION,
+} from '@/src/constants';
+
+export const companyData = {
+  companyName: COMPANY_NAME,
+  companyOwner: COMPANY_OWNER,
+  companyEmail: COMPANY_EMAIL,
+  companyPhoneMain: COMPANY_PHONE_MAIN,
+  companyPhoneCell: COMPANY_PHONE_CELL,
+  companyAddressStreet: COMPANY_ADDRESS_STREET,
+  companyAddressCity: COMPANY_ADDRESS_CITY,
+  companyAdressProvince: COMPANY_ADDRESS_PROVINCE,
+  companyAddressPostal: COMPANY_ADDRESS_POSTAL,
+  companyDescription: COMPANY_DESCRIPTION,
+};
