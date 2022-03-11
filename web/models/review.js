550750b7d3875bde998cf875313dd7dedd084bce
Colin Richardson <colin.richardson.dev@gmail.com>
2022-03-11 10:58:48 -0600

    refactors to separate query statement filters

--- web/models/review.js
+++ web/models/review.js
@@ -1,31 +1,39 @@
 import sanityClient from '../sanityClient';
 
-export const fetch = async ({ page, perPage = 10, order }) => {
+// common params used to filter GROQ query statements
+const parseParams = ({ page, perPage = 10, order }) => {
+  // check for params
+  const hasPage = page !== undefined;
+  const hasOrder = order !== undefined;
+  // sanity ranges begin at 0
+  const rangeFrom = hasPage ? (Number.parseInt(page) - 1) * perPage : -1;
+  const rangeTo = hasPage ? Number.parseInt(page) * perPage : -1;
+  // query statements
+  const rangeWithin = hasPage ? `[${rangeFrom}...${rangeTo}]` : '';
+  const orderBy = hasOrder ? ` | order(${order})` : '';
+
+  return {
+    rangeWithin,
+    orderBy,
+  };
+};
+
+export const fetchReviews = async (params) => {
   const result = { data: [], status: '' };
   try {
-    // params
-    const hasPage = page !== undefined;
-    const hasOrder = order !== undefined;
-    // sanity ranges begin at 0
-    const rangeFrom = hasPage ? (Number.parseInt(page) - 1) * perPage : -1;
-    const rangeTo = hasPage ? Number.parseInt(page) * perPage : -1;
-    // optional query statements
-    const range = hasPage ? `[${rangeFrom}...${rangeTo}]` : '';
-    const orderBy = hasOrder ? ` | order(${order})` : '';
-
+    const { orderBy, rangeWithin } = parseParams(params);
     // build result object
     result.data = await sanityClient.fetch(`
       *[_type == "review"]
       ${orderBy}
-      ${range}
+      ${rangeWithin}
     `);
     result.status = 'success';
 
     return result;
   } catch (error) {
-    console.error(error);
-    result.status = 'error';
-
+    console.error(`Sanity Error --- ${error}`);
+    result.status = error;
     return result;
   }
 };
