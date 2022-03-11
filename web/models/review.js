c10b27bcf2ae21044b134b01b1c026611ae3340a
Colin Richardson <colin.richardson.dev@gmail.com>
2022-03-11 11:41:04 -0600

    adds filterFor clause

--- web/models/review.js
+++ web/models/review.js
@@ -1,19 +1,22 @@
 import sanityClient from '../sanityClient';
 
 // common params used to filter GROQ query statements
-const parseParams = ({ page, perPage = 10, order }) => {
+const parseParams = ({ page, perPage = 10, filter, order }) => {
   // check for params
   const hasPage = page !== undefined;
+  const hasFilter = filter !== undefined;
   const hasOrder = order !== undefined;
   // sanity ranges begin at 0
   const rangeFrom = hasPage ? (Number.parseInt(page) - 1) * perPage : -1;
   const rangeTo = hasPage ? Number.parseInt(page) * perPage : -1;
   // query statements
   const rangeWithin = hasPage ? `[${rangeFrom}...${rangeTo}]` : '';
+  const filterFor = hasFilter ? ` && ${filter}` : '';
   const orderBy = hasOrder ? ` | order(${order})` : '';
 
   return {
     rangeWithin,
+    filterFor,
     orderBy,
   };
 };
@@ -21,10 +24,10 @@ const parseParams = ({ page, perPage = 10, order }) => {
 export const fetchReviews = async (params) => {
   const result = { data: [], status: '' };
   try {
-    const { orderBy, rangeWithin } = parseParams(params);
+    const { rangeWithin, filterFor, orderBy } = parseParams(params);
     // build result object
     result.data = await sanityClient.fetch(`
-      *[_type == "review"]
+      *[_type == "review"${filterFor}]
       ${orderBy}
       ${rangeWithin}
     `);
