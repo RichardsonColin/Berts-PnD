6694dae46d34d7c0bb8e28265a14c6e3896a0aec
Colin Richardson <colin.richardson.dev@gmail.com>
2022-03-11 19:58:39 -0600

    adds api endpoint for portfolio images

---
+++ web/pages/api/portfolioImages.js
@@ -0,0 +1,36 @@
+// models
+import { fetchPortfolioImages } from '@/models/portfolioImage';
+/*
+  Path:
+    /api/portfolioImages
+
+  Accepted Methods:
+    GET
+*/
+export default async function handler(req, res) {
+  try {
+    if (req.method === 'GET') {
+      // handle request
+      const { page, perPage, order } = req.query;
+      const { data, status } = await fetchPortfolioImages({
+        page,
+        perPage,
+        order,
+      });
+
+      // send response
+      if (status === 'success') {
+        res.status(200).json(data);
+      } else {
+        console.error(`HTTP Method: ${req.method} --- ${status}`);
+        res.status(400).json(data);
+      }
+    } else {
+      console.log(`HTTP Method: ${req.method} --- Unsupported HTTP Method`);
+      res.status(400).json([]);
+    }
+  } catch (error) {
+    console.error(`HTTP Method: ${req.method} --- ${error}`);
+    res.status(500).json([]);
+  }
+}
