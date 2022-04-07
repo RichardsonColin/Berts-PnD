812d33535a67e5913bcca82f513db27fe8182a20
Colin Richardson <colin.richardson.dev@gmail.com>
2022-04-07 11:27:00 -0500

    refactors to add check first

--- web/src/helpers.js
+++ web/src/helpers.js
@@ -1,13 +1,11 @@
 export function searchString(string, regExpPattern) {
   try {
-    if (typeof string === 'string' && string.length) {
-      const regex = new RegExp(regExpPattern);
-      const result = regex.exec(string);
-      // first index of a RegExp.exec return value is the matched expression
-      return result !== null ? result[0] : result;
-    }
-    // default value for unmatched RegExp.exec expression
-    return null;
+    if (typeof string !== 'string' && !string.length) return null;
+
+    const regex = new RegExp(regExpPattern);
+    const result = regex.exec(string);
+    // first index of a RegExp.exec return value is the matched expression
+    return result !== null ? result[0] : result;
   } catch (error) {
     console.error(error);
     return null;
