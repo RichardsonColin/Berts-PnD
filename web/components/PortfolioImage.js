96d9b5ccb9a64ecb16018177cbb8121265346614
Colin Richardson <colin.richardson.dev@gmail.com>
2022-05-26 16:24:08 -0500

    add portfolio image component that consumes a SanityImage

---
+++ web/components/PortfolioImage.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+// components
+import SanityImage from '@/components/SanityImage';
+
+PortfolioImage.propTypes = {
+  image: PropTypes.object.isRequired,
+  altText: PropTypes.string.isRequired,
+  layout: PropTypes.string.isRequired,
+  objectFit: PropTypes.string.isRequired,
+  quality: PropTypes.number,
+  blur: PropTypes.number,
+  className: PropTypes.string,
+};
+
+export default function PortfolioImage({
+  image,
+  altText,
+  layout,
+  objectFit,
+  quality = 80,
+  blur = 20,
+  className = '',
+}) {
+  return (
+    <SanityImage
+      image={image}
+      layout={layout}
+      objectFit={objectFit}
+      quality={quality}
+      blur={blur}
+      altText={altText}
+      className={className}
+    />
+  );
+}
