31bdbd05d3a35a85493285523dd87dcdf85757ca
Colin Richardson <colin.richardson.dev@gmail.com>
2022-07-17 18:52:27 -0500

    include recaptcha site key

--- web/next.config.js
+++ web/next.config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   env: {
     SANITY_PROJECT_ID: process.env['SANITY_PROJECT_ID'],
+    RECAPTCHA_SITE_KEY: process.env['RECAPTCHA_SITE_KEY'],
   },
   experimental: {
     // ssr and displayName are configured by default
