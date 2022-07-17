882c7cf431b38c6610badef284cd0abaef9e9353
Colin Richardson <colin.richardson.dev@gmail.com>
2022-07-17 18:57:50 -0500

    add reCAPTCHA for form use

---
+++ web/components/ReCaptcha.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+// components
+import ReCAPTCHA from 'react-google-recaptcha';
+// hooks
+import useMediaQuery from '@/hooks/useMediaQuery';
+// style
+import styled from 'styled-components';
+
+ReCaptcha.propTypes = {
+  reCaptchaRef: PropTypes.any,
+  reCaptchaError: PropTypes.string,
+  setToken: PropTypes.func.isRequired,
+  tabIndex: PropTypes.number,
+};
+
+export default function ReCaptcha({
+  reCaptchaRef,
+  reCaptchaError,
+  setToken,
+  tabIndex,
+}) {
+  const [message, setMessage] = useState('');
+  const isBreakPoint = useMediaQuery('mobileL');
+
+  useEffect(() => {
+    setMessage(reCaptchaError);
+  }, [reCaptchaError]);
+
+  const onChange = (value) => {
+    setToken(value);
+  };
+
+  const onErrored = () => {
+    setMessage(
+      'A reCAPTCHA issue occured, please refresh the page and try again.'
+    );
+  };
+
+  return (
+    <>
+      <ReCAPTCHA
+        ref={reCaptchaRef}
+        sitekey={process.env['RECAPTCHA_SITE_KEY']}
+        onChange={onChange}
+        onErrored={onErrored}
+        size={isBreakPoint ? 'compact' : 'normal'}
+        tabindex={tabIndex}
+      />
+      {message && <ReCaptchaMessage>{message}</ReCaptchaMessage>}
+    </>
+  );
+}
+
+const ReCaptchaMessage = styled.span`
+  color: var(--color-error);
+`;
