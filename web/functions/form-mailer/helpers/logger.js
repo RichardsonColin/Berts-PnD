c787acc62215eb22e403f723f4eb463b06e552b7
Colin Richardson <colin.richardson.dev@gmail.com>
2022-07-31 14:47:50 -0500

    include cache logger

--- web/functions/form-mailer/helpers/logger.js
+++ web/functions/form-mailer/helpers/logger.js
@@ -1,3 +1,8 @@
+const {
+  uploadS3JsonObject,
+  buildS3JsonUploadParams,
+} = require('../../../lib/s3/helpers');
+
 // structure the format for basic logging
 exports.logger = (message, type = 'info') => {
   const dateString = new Date().toISOString();
@@ -7,4 +12,14 @@ exports.logger = (message, type = 'info') => {
   if (type === 'error') type = '[ERROR]';
 
   console.log(`${dateString} ${type} ${message}`);
+};
+
+exports.cacheLogger = async (json, type) => {
+  const params = buildS3JsonUploadParams(
+    process.env.AWS_LOGS_BUCKET,
+    `logs-mail/${new Date().getTime()}-${type}.json`,
+    json
+  );
+  const result = await uploadS3JsonObject(params);
+  this.logger(result?.message);
 };
