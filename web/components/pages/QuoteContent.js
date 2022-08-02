98e41c149fadc6c2192fa7a5b676226dbf7ba154
Colin Richardson <colin.richardson.dev@gmail.com>
2022-08-02 14:44:37 -0500

    add corner stylings

--- web/components/pages/QuoteContent.js
+++ web/components/pages/QuoteContent.js
@@ -7,6 +7,8 @@ import { PagesHeadingWrapper as HeadingWrapper } from './styled/PagesHeading';
 import QuoteRequestForm from '@/components/QuoteRequestForm';
 import Gutter from '@/components/ui/Gutter';
 import Heading from '@/components/ui/Heading';
+// constants
+import { mediaQueries } from '@/utils/constants';
 
 QuoteContent.propTypes = {
   id: PropTypes.string.isRequired,
@@ -24,7 +26,9 @@ export default function QuoteContent({ id, heading, title, subtitle }) {
             <span>{subtitle}</span>
             <Heading level='2'>{title}</Heading>
           </HeadingWrapper>
-          <QuoteRequestForm />
+          <ContentWrapper>
+            <StyledQuoteRequestForm />
+          </ContentWrapper>
         </Gutter>
       </StyledQuoteSection>
     </PagesContent>
@@ -35,4 +39,52 @@ export default function QuoteContent({ id, heading, title, subtitle }) {
 const StyledQuoteSection = styled(StyledSection)`
   text-align: center;
   margin: auto;
+`;
+const ContentWrapper = styled.div`
+  ${StyledQuoteSection} & {
+    position: relative;
+    max-width: 1000px;
+    margin: 0 auto;
+    background-color: var(--secondary);
+    z-index: 0;
+
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: -10px;
+      left: -10px;
+      width: 150px;
+      height: 150px;
+      background-color: var(--primary);
+      box-shadow: 0 0 2px #000;
+      z-index: -1;
+    }
+    &:after {
+      content: '';
+      position: absolute;
+      top: -10px;
+      right: -10px;
+      width: 150px;
+      height: 150px;
+      background-color: var(--primary);
+      box-shadow: 0 0 2px #000;
+      z-index: -1;
+    }
+
+    @media (min-width: ${mediaQueries.laptop}) {
+      &:before {
+        bottom: -20px;
+        left: -20px;
+      }
+      &:after {
+        top: -20px;
+        right: -20px;
+      }
+    }
+  }
+`;
+const StyledQuoteRequestForm = styled(QuoteRequestForm)`
+  ${StyledQuoteSection} & {
+    z-index: 1;
+  }
 `;
