e9e35b21e97f9448a0b921bfdc44dd9aa333d836
Colin Richardson <colin.richardson.dev@gmail.com>
2022-08-02 15:13:03 -0500

    change env vars from netlify reserved vars

--- web/lib/s3/client.js
+++ web/lib/s3/client.js
@@ -2,9 +2,9 @@ const process = require('process');
 const { S3Client } = require('@aws-sdk/client-s3');
 
 exports.s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_SITE_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_SITE_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SITE_SECRET_ACCESS_KEY,
   },
 });
--- web/lib/ses/client.js
+++ web/lib/ses/client.js
@@ -2,7 +2,7 @@ const process = require('process');
 const { SESClient } = require('@aws-sdk/client-ses');
 
 exports.sesClient = new SESClient({
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_SITE_REGION,
+  accessKeyId: process.env.AWS_SITE_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SITE_SECRET_ACCESS_KEY,
 });
