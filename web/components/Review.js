5a3e93a6d05566ce68a00bd9a973d9063c0601c4
Colin Richardson <colin.richardson.dev@gmail.com>
2022-08-22 18:27:00 -0500

    modify to secondary-fade bg

--- web/components/Review.js
+++ web/components/Review.js
@@ -46,10 +46,11 @@ const StyledAuthor = styled.p`
       content: '';
       position: absolute;
       left: 50%;
-      transform: translateX(-50%) rotate(45deg);
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%) rotate(45deg);
       width: 35px;
       height: 35px;
-      background-color: var(--primary-light);
+      background-color: var(--secondary-fade);
       opacity: 0.8;
       z-index: -1;
     }
