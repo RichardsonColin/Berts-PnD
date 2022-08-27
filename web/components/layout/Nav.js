4392697c7dd44e0779a5d55821407fe798e7a9e1
Colin Richardson <colin.richardson.dev@gmail.com>
2022-08-27 13:39:59 -0500

    reduce need for JS to switch between nav and burg

--- web/components/layout/Nav.js
+++ web/components/layout/Nav.js
@@ -3,7 +3,6 @@ import FocusLock from 'react-focus-lock';
 import styled from 'styled-components';
 // hooks
 import useMediaQuery from '@/hooks/useMediaQuery';
-import useHasMounted from '@/hooks/useHasMounted';
 // components
 import NavList from './NavList';
 import Burger from '@/components/layout/Burger';
@@ -12,38 +11,42 @@ import BurgerMenu from '@/components/layout/BurgerMenu';
 import { mediaQueries } from '@/utils/constants';
 
 export default function Nav() {
-  const hasMounted = useHasMounted();
   const node = useRef();
   const [open, setOpen] = useState(false);
   const isLargeViewport = useMediaQuery(`(min-width: ${mediaQueries.laptop})`);
 
-  if (!hasMounted) {
-    return null;
+  if (isLargeViewport && open) {
+    setOpen(false);
   }
 
   return (
     <>
-      {!isLargeViewport ? (
-        <MobileNavWrapper ref={node}>
-          <FocusLock disabled={!open}>
-            <Burger open={open} setOpen={setOpen} />
-            <BurgerMenu open={open} setOpen={setOpen} />
-          </FocusLock>
-        </MobileNavWrapper>
-      ) : (
-        <StyledNav role='navigation' aria-label='Main'>
-          <NavList />
-        </StyledNav>
-      )}
+      <MobileNavWrapper ref={node}>
+        <FocusLock disabled={!open}>
+          <Burger open={open} setOpen={setOpen} />
+          <BurgerMenu open={open} setOpen={setOpen} />
+        </FocusLock>
+      </MobileNavWrapper>
+      <StyledNav role='navigation' aria-label='Main'>
+        <NavList />
+      </StyledNav>
     </>
   );
 }
 
 // styles
-const MobileNavWrapper = styled.div``;
+const MobileNavWrapper = styled.div`
+  @media (min-width: ${mediaQueries.laptop}) {
+    display: none;
+  }
+`;
 const StyledNav = styled.nav`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
+  display: none;
   height: inherit;
+
+  @media (min-width: ${mediaQueries.laptop}) {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
 `;
